a31c856af55e3c28e3a448fc2cbacf752cb4448f
llleeeaaannn <piercefrank10@gmail.com>
2023-02-28 23:43:40 +0000

    Created card content without styling

--- src/components/card.js
+++ src/components/card.js
@@ -6,7 +6,49 @@ const Card = ({ data }) => {
 
   return (
     <div className={styles.card}>
-      
+      <header>
+        <h2>Economics and Business 2023</h2>
+      </header>
+
+      <main>
+        <img src='image' alt='erasmus logo'></img>
+
+        <div className={styles.headshot}>
+          <img src='image' alt='headshot'></img>
+        </div>
+
+        <div className={styles.details}>
+          <div>
+            <span>Student</span>
+            <p>F.D. Pierce</p>
+          </div>
+
+          <div>
+            <span>Date of Birth</span>
+            <p>13-07-1998</p>
+          </div>
+
+          <div>
+            <span>Student ID</span>
+            <p>596240</p>
+          </div>
+
+          <div>
+            <span>Degree Programme</span>
+            <p>Economics and Business</p>
+          </div>
+
+          <div>
+            <span>Academic Year</span>
+            <p>2022-2023</p>
+          </div>
+        </div>
+
+      </main>
+
+      <footer>
+        <span>Valid from 01-09-2022 to 31-08-2023</span>
+      </footer>
     </div>
   )
 }
