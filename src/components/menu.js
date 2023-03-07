3d4960eafb16fecc60e9f700757d6efb00fa1c80
llleeeaaannn <piercefrank10@gmail.com>
2023-03-07 00:02:09 +0000

    Created menu & help components

---
+++ src/components/menu.js
@@ -0,0 +1,13 @@
+import styles from '../styles/menu.module.css';
+import { useState, useEffect, useContext } from 'react';
+import { AppContext } from '../App';
+
+const Menu = () => {
+
+  return (
+    <div className={styles.menu}>
+    </div>
+  )
+}
+
+export default Menu;
